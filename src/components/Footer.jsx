f60a6ba2bcc4c6d5a6917c5e6e360a2edac85394
RamiroUrt <121266524+RamiroUrt@users.noreply.github.com>
2024-02-05 20:03:17 -0300

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,13 +13,13 @@ const Fotter = () => {
         <div className=" py-0 max-w-[100%] flex flex-wrap justify-center flex-col items-center overflow-hidden">
                     <img src="IMG/logo.jpg" alt="Logo de SLee Dw" className="w-[250px]" />
         </div>
-        <div className="max-w-[100%] flex flex-wrap justify-center flex-col items-center m-[2rem] text-gray-500 text-xl text-center leading-[2.5rem]">
-            <h2 className='flex overflow-hidden flex-wrap break-all font-bold leading-[3rem] md:leading-[7rem] text-primary '>SOBRE NOSOTROS</h2>
-            <p className='flex overflow-hidden flex-wrap break-all '>Dirección: 123 Calle Principal, Ciudad, País</p>
-            <p className='flex overflow-hidden flex-wrap break-all '>Email: info@bikeshop.com</p>
-            <p className='flex overflow-hidden flex-wrap break-all '>Teléfono: +123 456 789</p>
+        <div className="max-w-[100%] flex flex-wrap justify-center flex-col items-center m-[2rem] text-gray-500 text-wrap text-xl text-center leading-[2.5rem]">
+            <h2 className='flex overflow-hidden  font-bold leading-[3rem] md:leading-[7rem] text-primary '>SOBRE NOSOTROS</h2>
+            <p className='flex overflow-hidden  '>Dirección: 123 Calle Principal, Ciudad, País</p>
+            <p className='flex overflow-hidden  '>Email: info@bikeshop.com</p>
+            <p className='flex overflow-hidden  '>Teléfono: +123 456 789</p>
         </div>
-        <div className="max-w-[100%] flex flex-wrap justify-center flex-col items-center overflow-hidden m-[2rem] text-xl pt-0 mt-0 leading-[3rem]">
+        <div className="max-w-[100%] flex flex-wrap justify-center flex-col items-center overflow-hidden m-[2rem] text-xl text-center pt-0 mt-0 leading-[3rem]">
             <h2 className='flex overflow-hidden flex-wrap break-all font-bold leading-[3rem] md:leading-[7rem] text-primary pt-0 mt-0 pb-[60px]'>SIGUENOS</h2>
             <div className="flex flex-wrap aling-center items-center text-center justify-center felx-col list-none">
                 <li className='pl-5'>                 
